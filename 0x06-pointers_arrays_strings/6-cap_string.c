13031a95b17b54bdc37fbe2d4c45c4355f425f6c
OgidiNkemjika <ogidinkemjika@gmail.com>
2022-10-26 13:20:54 -0700

    Write a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,31 @@
 #include "main.h"
 
 /**
- * leet - encodes a string in 1337
- * @s: string to be encoded
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
  *
- * Return: the resulting string;
+ * Return: the resulting string
  */
-char *leet(char *s)
+char *cap_string(char *s)
 {
 	int i, j;
 
-	char *a = "aAeEoOtTlL";
-	char *b = "4433007711";
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == a[j])
+			if (s[i] == spe[j])
 			{
-				s[i] = b[j];
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
 			}
 		}
 	}
